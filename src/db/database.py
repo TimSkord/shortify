200d227d84d3cf8cd455f345f0d7020baa66f223
TimSkoro <skotwind@gmail.com>
2023-11-24 00:45:35 +0400

    feat: alembic was setup

--- src/db/database.py
+++ src/db/database.py
@@ -1,8 +1,7 @@
+from config import REQUEST_DB_CONNECTION_STRING
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-from config import REQUEST_DB_CONNECTION_STRING
 
 SQLALCHEMY_DATABASE_URL = REQUEST_DB_CONNECTION_STRING
 
